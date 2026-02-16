f70a657f49848262f01458915cc85464f258e724
Malvin <malvinbl@gmail.com>
2026-02-16 18:11:25 +0100

    chore: estructura inicial

---
+++ src/main/java/es/mblcu/videomanager/Application.java
@@ -0,0 +1,12 @@
+package es.mblcu.videomanager;
+
+public final class Application {
+
+    private Application() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== VIDEOMANAGER STARTED ===");
+    }
+
+}
---
+++ src/test/java/es/mblcu/videomanager/ApplicationTest.java
@@ -0,0 +1,14 @@
+package es.mblcu.videomanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldRun() {
+        assertTrue(true);
+    }
+
+}
